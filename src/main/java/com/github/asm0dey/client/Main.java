29f2b32530a4fb9c5c9ce7bc432499f6b6b36780
finkel <pavel.finkelshtein@gmail.com>
2013-03-15 00:59:28 +0400

    initial commit

---
+++ src/main/java/com/github/asm0dey/client/Main.java
@@ -0,0 +1,16 @@
+package com.github.asm0dey.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * User: finkel
+ * <p/>
+ * Date: 15.03.13
+ * <p/>
+ * Time: 0:36
+ */
+public class Main implements EntryPoint {
+    public void onModuleLoad() {
+
+    }
+}
